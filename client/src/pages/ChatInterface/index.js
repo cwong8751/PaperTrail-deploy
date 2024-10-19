b3416ebe3ccd2af60cc4a1187b0a4bdeab555898
David Cui <112504777+boncui@users.noreply.github.com>
2024-10-19 15:41:24 -0500

    deleted client folder

--- client/src/pages/ChatInterface/index.js
+++
@@ -1,7 +0,0 @@
-// https://api.mapbox.com/{endpoint}?access_token={your_access_token}
-import ChatInterface from "./ChatWIthAI";
-import RealTimeOCR from "./RealTimeOCR";
-export  {
-    ChatInterface,
-    RealTimeOCR
-}
